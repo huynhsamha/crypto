aa547195daf27f075f89a1526b8abf5c93c94b29
Huynh Ha <huynhsamha@gmail.com>
2018-08-06 12:03:04 +0700

    add feature (length + encoder)

--- src/components/algorithm/Algorithm.jsx
+++ src/components/algorithm/Algorithm.jsx
@@ -4,13 +4,11 @@ import { connect } from 'react-redux';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Fade from 'react-reveal/Fade';
 import Bounce from 'react-reveal/Bounce';
-
-import cryptoEncoderUtf8 from 'crypto-js/enc-utf8';
+import { enc as cryptoEncoder } from 'crypto-js';
 
 import * as actions from '../../redux/actions';
 
 import './Algorithm.css';
-
 
 class Algorithm extends Component {
 
@@ -20,11 +18,18 @@ class Algorithm extends Component {
     this.state = {
       data: '',
       key: '',
-      decryptKey: ''
+      decryptKey: '',
+      encoder: cryptoEncoder.Hex
     };
 
     const { algo } = this.props;
     document.title = algo.name.toUpperCase() + (algo.detail ? ` - ${algo.detail}` : '');
+
+    this.constants = {
+      'ONE_WAY_NO_SECRET': 'ONE_WAY_NO_SECRET',
+      'ONE_WAY_WITH_SECRET': 'ONE_WAY_WITH_SECRET',
+      'ENCRYPT_DECRYPT': 'ENCRYPT_DECRYPT'
+    };
   }
 
   handleOnCopy = () => {
@@ -32,28 +37,38 @@ class Algorithm extends Component {
     showAlert('Copied to clipboard');
   }
 
-  render() {
-    const { data, key, decryptKey } = this.state;
+  retrieveType = () => {
     const { algo } = this.props;
+    if (!algo.key) return this.constants.ONE_WAY_NO_SECRET;
+    if (!algo.decrypt) return this.constants.ONE_WAY_WITH_SECRET;
+    return this.constants.ENCRYPT_DECRYPT;
+  }
+
+  render() {
+    const { data, key, decryptKey, encoder } = this.state;
+    const { algo } = this.props;
+    const type = this.retrieveType();
 
     let encryptValue = '';
     let decryptValue = '';
     if (data) {
-      if (!algo.key) {
-        // handle one-way algo without secret key
-        encryptValue = algo.func(data);
-      } else if (!algo.decrypt) {
-        // handle one-way algo using secret key
-        encryptValue = algo.func(data, key);
-      } else {
-        // handle encryption & decryption algo
-        encryptValue = algo.func.encrypt(data, key);
-        try {
+      try {
+        if (type === this.constants.ONE_WAY_NO_SECRET) {
+          // handle one-way algo without secret key
+          encryptValue = algo.func(data).toString(encoder);
+        } else if (type === this.constants.ONE_WAY_WITH_SECRET) {
+          // handle one-way algo using secret key
+          encryptValue = algo.func(data, key).toString(encoder);
+        } else {
+          // handle encryption & decryption algo
+          encryptValue = algo.func.encrypt(data, key).toString();
           // handle error malware utf8
-          decryptValue = algo.func.decrypt(encryptValue.toString(), decryptKey).toString(cryptoEncoderUtf8);
-        } catch (err) {
-          decryptValue = '';
+          decryptValue = algo.func.decrypt(encryptValue, decryptKey).toString(cryptoEncoder.Utf8);
         }
+      } catch (err) {
+        console.log(err.message);
+        encryptValue = '';
+        decryptValue = '';
       }
     }
 
@@ -87,7 +102,7 @@ class Algorithm extends Component {
         </div>
 
         {/* Using secret key */}
-        {algo.key && (
+        {type !== this.constants.ONE_WAY_NO_SECRET && (
           <div className="form-group row">
             <Fade>
               <label htmlFor="js-user-key">Secret Key</label>
@@ -105,23 +120,42 @@ class Algorithm extends Component {
           <Fade>
             <label htmlFor="js-result">{algo.decrypt && 'Encrypted'} Result</label>
             <textarea
-              className="form-control" rows="4" id="js-result" disabled
+              className="form-control" rows="6" id="js-result" disabled
               value={encryptValue}
             />
-            <CopyToClipboard
-              text={encryptValue}
-              onCopy={this.handleOnCopy}
-            >
-              <button className="btn btn-primary btn-sm position-absolute btn-copy" type="button">
-                Copy <i className="fa fa-copy" />
+
+            {type !== this.constants.ENCRYPT_DECRYPT ? (
+              <div className="side-left">
+                {Object.keys(cryptoEncoder).map(name => name !== 'Utf8' && name !== 'Utf16BE' && (
+                  <button
+                    className={`btn btn-primary btn-sm btn-custom btn-encoder ${cryptoEncoder[name] === encoder ? 'active' : ''}`}
+                    type="button" key={name}
+                    onClick={() => this.setState({ encoder: cryptoEncoder[name] })}
+                  >
+                    {name}
+                  </button>
+                ))}
+              </div>
+            ) : <div />}
+            <div className="side-right">
+              <button className="btn btn-primary btn-sm btn-custom btn-length" type="button">
+                Length: {encryptValue.length}
               </button>
-            </CopyToClipboard>
+              <CopyToClipboard
+                text={encryptValue}
+                onCopy={this.handleOnCopy}
+              >
+                <button className="btn btn-primary btn-sm btn-custom btn-copy" type="button">
+                    Copy <i className="fa fa-copy" />
+                </button>
+              </CopyToClipboard>
+            </div>
           </Fade>
 
         </div>
 
         {/* Encryption & Decryption */}
-        {algo.decrypt && (
+        {type === this.constants.ENCRYPT_DECRYPT && (
           <div>
             <div className="form-group row">
               <Fade>
